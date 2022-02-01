65e05f186fa2b84a8579eff7257521f367959ab1
Minh <57801022+hminh2027@users.noreply.github.com>
2022-02-01 13:54:50 +0700

    Update Axios.js

--- src/apis/Axios.js
+++ src/apis/Axios.js
@@ -1,35 +1,37 @@
 import axios from 'axios'
 
+const domainServer = 'https://nodejs-dev-up.herokuapp.com/'
+
 export const postAPI = async (url, data, token) => {
-    const res = await axios.post(`/api/${url}`, data, {
+    const res = await axios.post(`${domainServer}/api/${url}`, data, {
         headers: { Authorization: token}
     })
     return res
 }
 
 export const getAPI = async (url, token) => {
-    const res = await axios.get(`/api/${url}`, {
+    const res = await axios.get(`${domainServer}/api/${url}`, {
         headers: {Authorization: token}
     })
     return res
 }
 
 export const putAPI = async (url, data, token) => {
-    const res = await axios.put(`/api/${url}`, data, {
+    const res = await axios.put(`${domainServer}/api/${url}`, data, {
         headers: {Authorization: token}
     })
     return res
 }
 
 export const patchAPI = async (url, data, token) => {
-    const res = await axios.patch(`/api/${url}`, data, {
+    const res = await axios.patch(`${domainServer}/api/${url}`, data, {
         headers: {Authorization: token}
     })
     return res
 }
 
 export const deleteAPI = async (url, token) => {
-    const res = await axios.delete(`/api/${url}`, {
+    const res = await axios.delete(`${domainServer}/api/${url}`, {
         headers: {Authorization: token}
     })
     return res
